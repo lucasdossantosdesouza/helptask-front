ce00b3c86d4584b06e8ab3920cd7ab0a1844bd52
lucasdossantosdesouza <43444472+lucasdossantosdesouza@users.noreply.github.com>
2024-03-28 09:34:09 -0400

    update version angular

--- HelpTask/src/app/component/security/auth.guard.ts
+++ HelpTask/src/app/component/security/auth.guard.ts
@@ -1,12 +1,12 @@
 import { SharedService } from 'src/app/services/shared.service';
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
   
 
   public shared: SharedService;
